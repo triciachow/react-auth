096a89e90aca3150812675765bc5efe23616e44c
triciachow0212 <suyeechow0212@gmail.com>
2022-07-09 13:02:26 +0800

    Add comments

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 async function dbConnect() {
+  // Use mongoose to connect this app to our
+  // database on mongoDB using the DB_URL (connection string)
   mongoose
     .connect(process.env.DB_URL, {
+      // These are options to ensure that the connection is done properly
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
@@ -12,7 +15,7 @@ async function dbConnect() {
       console.log("Successfully connected to MongoDB Atlas!");
     })
     .catch(error => {
-      console.log(`Unable to connect to DB. Error: ${error}`);
+      console.log(`Unable to connect to DB. ${error}`);
     });
 }
 
