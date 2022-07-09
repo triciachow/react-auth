8552070dc543d1bf52357274419f9f7364bbb22e
triciachow0212 <suyeechow0212@gmail.com>
2022-07-09 18:34:59 +0800

    Add DB URL

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -6,11 +6,15 @@ async function dbConnect() {
   // Use mongoose to connect this app to our
   // database on mongoDB using the DB_URL (connection string)
   mongoose
-    .connect(process.env.DB_URL, {
-      // These are options to ensure that the connection is done properly
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      process.env.DB_URL ||
+        "mongodb+srv://tricia:Ksr3VyKuh1pq1fz9@users.dv5g0v5.mongodb.net/authDB?retryWrites=true&w=majority",
+      {
+        // These are options to ensure that the connection is done properly
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .then(() => {
       console.log("Successfully connected to MongoDB Atlas!");
     })
