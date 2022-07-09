574817e6384db442a4394cb09305f42ea2dde781
triciachow0212 <suyeechow0212@gmail.com>
2022-07-09 13:48:43 +0800

    Create login endpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const dbConnect = require("./db/dbConnect");
 const User = require("./db/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Body parser configuration
 app.use(bodyParser.json());
@@ -44,6 +45,52 @@ app.post("/register", (request, response) => {
     .catch(error => {
       response.status(500).send({
         message: "Password was not hashed successfully",
+        error,
+      });
+    });
+});
+
+app.post("/login", (request, response) => {
+  // Check if email exists
+  User.findOne({ email: request.body.email })
+    // If email exists
+    .then(user => {
+      bcrypt
+        .compare(request.body.password, user.password)
+        .then(passwordCheck => {
+          // Check if passwords match
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: "Password does not match",
+              error,
+            });
+          }
+          // If the passwords match, create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+          // Return success response
+          response.status(200).send({
+            message: "Login successful",
+            email: user.email,
+            token,
+          });
+        })
+        .catch(error => {
+          response.status(404).send({
+            message: "Password does not match",
+            error,
+          });
+        });
+    })
+    .catch(error => {
+      response.status(404).send({
+        message: "Email not found",
         error,
       });
     });
